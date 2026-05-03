062a903dc8eed40f974e0a4aed3dfbb8434dcc19
SnakeXrnt <ethanbastian11@gmail.com>
2026-05-03 18:36:28 +0200

    Template to merge to main

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+        <div className='home-page'>
+            <div className='left'>
+                <h1>Seasae</h1>
+                <h2>south east asotiation in netherlands</h2>
+            </div>
+            <div className='right'>
+                <h1>right box here</h1>
+                <h2>delete if you dont want </h2>
+                <h3> see the css, there is a border for every flexbox for easier debugging, delete this if you dont want it to be there ! </h3>
+
+            </div>  
+        </div>
+      )
+}
+
+export default App
