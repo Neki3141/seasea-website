9fc831421613d05b0b5c6badebdf6d7f724161a3
NW1728 <ethan@NW1728s-MacBook-Air.local>
2026-05-04 22:31:59 +0200

    feat : adding navbar template

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import './App.css'
+import Navbar from './component/navbar/navbar'
 
 function App() {
 
   return (
+    <div className='top'>
+        <Navbar />
         <div className='home-page'>
             <div className='left'>
-                <h1>Seasae</h1>
-                <h2>south east asotiation in netherlands</h2>
-            </div>
-            <div className='right'>
-                <h1>right box here</h1>
-                <h2>delete if you dont want </h2>
-                <h3> see the css, there is a border for every flexbox for easier debugging, delete this if you dont want it to be there ! </h3>
+                    <h1>Seasae</h1>
+                    <h2>south east asotiation in netherlands</h2>
+                </div>
+                <div className='right'>
+                    <h1>right box here</h1>
+                    <h2>delete if you dont want </h2>
+                    <h3> see the css, there is a border for every flexbox for easier debugging, delete this if you dont want it to be there ! </h3>
 
-            </div>  
+                </div>  
+            </div>
+ <div className='home-page'>
+            <div className='left'>
+                    <h1>Seasae</h1>
+                    <h2>south east asotiation in netherlands</h2>
+                </div>
+                <div className='right'>
+                    <h1>right box here</h1>
+                    <h2>delete if you dont want </h2>
+                    <h3> see the css, there is a border for every flexbox for easier debugging, delete this if you dont want it to be there ! </h3>
+
+                </div>  
+            </div>
         </div>
-      )
+              )
 }
 
 export default App
---
+++ src/component/navbar/navbar.tsx
@@ -0,0 +1,25 @@
+
+import './navbar.css'
+
+function Navbar() {
+    
+    return (
+        <nav className='navbar-box'>
+            <div className='left'>
+                <h1>LEFT</h1>
+            </div>
+            <div className='middle'>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+                <p>lorem</p>
+            </div>
+            <div className='right'>
+                <h1>RIGHT</h1>
+            </div>
+        </nav>
+    )
+    
+}
+
+export default Navbar
