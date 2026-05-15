2c348f0f6adabcf218de5e5803817cd6ede0ed4b
Neki3141 <trungkien14072007@gmail.com>
2026-05-15 23:41:12 +0200

    Chore: adding LICENSE.md
    1. Making intro template
    2. Licensing the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+/*!
+ * @license
+ * Copyright (C) 2026 Ethan Bastian, Kien Nguyen
+ * All Rights Reserved. Licensed under GNU AGPLv3.
+ * Full license text available in the source repository.
+ */
+
 import './App.css'
 import AboutUs from './component/about_us/about_us'
 import Intro from './component/introduction/intro'
